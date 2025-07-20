1ebb9b8c6f9ea4173f0fd135662af45368b6ede2
Lev Akchurin <levisserena@yandex.ru>
2025-07-21 02:06:20 +0400

    bagfix

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -13,7 +13,8 @@ window.onload = function() {
     plugins: [
       SwaggerUIBundle.plugins.DownloadUrl
     ],
-    layout: "StandaloneLayout"
+    layout: "StandaloneLayout",
+    validatorUrl: null
   });
 
   //</editor-fold>
